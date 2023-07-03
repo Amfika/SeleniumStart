d1564cc4608e7744b54b08272b479c63ae012ea7
Анастасия Сечко <asanastasia1@gmail.com>
2023-07-03 16:57:53 +0300

    add files

---
+++ src/test/java/selenium/tests/MyTest.java
@@ -0,0 +1,20 @@
+package selenium.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyTest {
+
+    @Test
+    public static void firstTest(){
+        WebDriver driver = new ChromeDriver(); //экземпляр web-драйвера
+        driver.get("https://the-internet.herokuapp.com");//открыть ссылку
+        WebElement links = driver.findElement(By.id("content"));
+        WebElement link = links.findElement(By.linkText("Drag and Drop"));
+        link.click();
+        //driver.quit();//закрыть браузер
+    }
+}
